be5e53072e0fbb3c094b06163439fa0b52f2e3c2
BetoSandoval <alberto.sa1391@gmail.com>
2022-02-20 23:45:24 -0600

    Nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/nav.css";
+import logo from "../assets/sabia-10x10-02.png";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <img src={logo} className="logo" alt="savia" />
+
+      <div className="menu">
+        <ul>
+          <li className="list-item">Sobre Nosotros</li>
+          <li className="list-item">Productos y Servicios</li>
+          <li className="list-item">Envios</li>
+          <li className="list-item">Contacto</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
