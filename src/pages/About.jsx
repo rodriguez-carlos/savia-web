37e8e36b0532bb861949a1b7adb2f56cccb08aa3
BetoSandoval <alberto.sa1391@gmail.com>
2022-03-03 21:10:41 -0600

    Creacion de pagina Sobre Nosotros

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const About = () => {
+  return (
+    <div>
+      <Nav />
+      <h1>Aqui va la pagina sobre nosotros</h1>
+    </div>
+  );
+};
+
+export default About;
