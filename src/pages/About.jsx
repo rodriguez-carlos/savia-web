1b06c6834bacd0b4e35f796dfca51dd7faf06925
BetoSandoval <alberto.sa1391@gmail.com>
2022-03-06 01:03:55 -0600

    Agregue Footer al componente About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const About = () => {
   return (
     <div>
       <Nav />
       <h1>Aqui va la pagina sobre nosotros</h1>
+      <Footer />
     </div>
   );
 };
