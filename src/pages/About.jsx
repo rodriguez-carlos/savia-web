58a27e4aa317a4e3b00ff81dd84bb9a3d5a2ecc9
BetoSandoval <alberto.sa1391@gmail.com>
2022-03-06 19:53:36 -0600

    efectos delemntos pagina Sobre Nosotros

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import  '../styles/Sass/aboutPage.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-
+AOS.init();
 const About = () => {
   return (
     <div>
@@ -14,24 +16,24 @@ const About = () => {
 
           <div className="grey-one">
           </div>
-          <div className="white-one">
+          <div className="white-one" data-aos="fade-right">
             <h3>Lorem ipsum dolor</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique quisquam explicabo sit nam doloribus modi dignissimos! Praesentium officiis cum ad in et? Autem unde, ducimus repudiandae commodi laudantium enim!</p>
           </div>
 
-          <div className="grey-two">
+          <div className="grey-two" data-aos="fade-left">
           </div>
 
-          <div className="grey-three">
+          <div className="grey-three" data-aos="fade-right">
           </div>
-          <div className="white-two">
+          <div className="white-two" data-aos="fade-right">
             <h3>Lorem ipsum dolor</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique quisquam explicabo sit nam doloribus modi dignissimos! Praesentium officiis cum ad in et? Autem unde, ducimus repudiandae commodi laudantium enim!</p>
           </div>
 
-          <div className="grey-four">
+          <div className="grey-four" data-aos="fade-right">
           </div>
-          <div className="white-three">
+          <div className="white-three" data-aos="fade-left">
             <h3>Lorem ipsum dolor</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique quisquam explicabo sit nam doloribus modi dignissimos! Praesentium officiis cum ad in et? Autem unde, ducimus repudiandae commodi laudantium enim!</p>
           </div>
