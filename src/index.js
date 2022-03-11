40a6b32c5fee821bbd87d9bd70b856888450808a
BetoSandoval <alberto.sa1391@gmail.com>
2022-03-11 17:38:58 -0600

    Correción al nombre de cambio de rutas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import Rutas from "./rutes";
+import Rutas from "./routes";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
