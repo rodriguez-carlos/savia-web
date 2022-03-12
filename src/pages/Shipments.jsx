90eda97572ac59f0dc9d9dd3f6fbf97b7bdb5d7f
BetoSandoval <alberto.sa1391@gmail.com>
2022-03-12 17:53:07 -0600

    Responsive design

--- src/pages/Shipments.jsx
+++ src/pages/Shipments.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Nav from '../components/Nav'
+import React from "react";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
 
 const Shimpments = () => {
   return (
     <div>
-        <Nav />
-        <div>
-            Aqui va la pagina de envios
-        </div>
-        <Footer />
+      <Nav />
+      <div>
+        <h1 className="title-one">Hacemos envíos gratis en</h1>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Shimpments;
