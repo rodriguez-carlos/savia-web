f7942942e4b8fe46a3a4fd039b6ee7b22ab8afee
unknown <carlosdanielrod@gmail.com>
2022-03-16 22:55:15 -0500

    alerta de edad y algunos tamaños de fuente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Footer from "../components/Footer";
 import ShowcaseCarousel from "../components/ShowcaseCarousel";
 import ProductCardsContainer from "../components/ProductCardsContainer";
 import ShowcaseCarouselItem from "../components/ShowcaseCarouselItem";
+import AgeAlert from "../components/AgeAlert";
 
 const Home = () => {
   return (
     <>
       <Nav />
+      <AgeAlert />
       <ShowcaseCarousel />
       <ProductCardsContainer />
       <ShowcaseCarouselItem textOnLeft />
