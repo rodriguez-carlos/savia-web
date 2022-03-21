7368bd89835ff3fba8715cce3d8a8a716d831c81
BetoSandoval <alberto.sa1391@gmail.com>
2022-03-20 22:43:54 -0600

    Maquetacion variedades desktop terminada

---
+++ src/pages/BeersKinds.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import ProductCard from "../components/ProductCard";
+import Button from "@mui/material/Button";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+
+const BeersKinds = () => {
+  return (
+    <>
+      <Nav />
+      <div className="description-photo">
+        <div className="description">
+          <h1 className="title">Lorem ipsum dolor</h1>
+          <p className="subtitle">Lorem ipsum dolor</p>
+
+          <p className="packs-name">Loerm</p>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            fuga libero sequi impedit dignissimos possimus dolore tenetur
+            reiciendis accusantium quia. Blanditiis quisquam officiis soluta
+            deserunt quo odio facilis nostrum odit.
+          </p>
+
+          <p className="subtitle-three">Ingredientes:</p>
+          <p className="text-body">
+            <span>lorem:</span> Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit.
+          </p>
+          <p className="text-body">
+            <span>lorem:</span> Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit.
+          </p>
+          <p className="text-body">
+            <span>lorem:</span> Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit.
+          </p>
+
+          <hr />
+
+          <p className="text-body-porcent">
+            lorem: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+          <p className="text-body-porcent">
+            lorem: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+          <p className="text-body-porcent">
+            lorem: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+
+          <p className="precio"><span>Precio:</span></p>
+          <Button id="button" sx={{ borderRadius: 15, marginTop: 1 }}>
+            Consultar
+          </Button>
+          <p className="precio"><span>Precio:</span></p>
+          <Button id="button" sx={{ borderRadius: 15, marginTop: 1 }}>
+            Consultar
+          </Button>
+          <p className="precio"><span>Precio:</span></p>
+          <Button id="button" sx={{ borderRadius: 15, marginTop: 1 }}>
+            Consultar
+          </Button>
+        </div>
+        <div className="photo"></div>
+      </div>
+
+      <p className="title-2">Mira todos nuestro packs</p>
+
+      <Swiper slidesPerView={5} spaceBetween={33} className="mySwiper">
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+      </Swiper>
+      <Footer />
+    </>
+  );
+};
+
+export default BeersKinds;
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,16 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Shimpments from "./pages/Shipments";
 import Contact from "./pages/Contact";
+import BeersKinds from "./pages/BeersKinds";
 
 function Rutas() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} exact />
+      <Route path="/" element={<Home />}/>
       <Route path="/nosotros" element={<About />} exact />
       <Route path="/envios" element={<Shimpments />} exact />
       <Route path="/contacto" element={<Contact />} exact />
+      <Route path="/variedades" element={<BeersKinds  />} exact />
     </Routes>
   );
 }
