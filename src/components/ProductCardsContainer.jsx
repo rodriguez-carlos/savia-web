b171618528309e1481e0d0d06a680c490e33c41a
unknown <carlosdanielrod@gmail.com>
2022-03-23 09:02:20 -0500

    arreglado ancho y font-size del Nav. Implementé un scroll preliminar para las cards

--- src/components/ProductCardsContainer.jsx
+++ src/components/ProductCardsContainer.jsx
@@ -7,20 +7,29 @@ const ProductCardsContainer = () => {
         <div className="product-cards-container">
             <Typography variant="h1" className="product-cards-container-title">Nuestras Cervezas</Typography>
             <Typography variant="h5" className="product-cards-container-text">Savia es cerveza 100% artesanal y chilena <br /> Te presentamos nuestras variedades, ¡pero no dejes de probarlas todas!</Typography>
-            <Grid className="product-cards-container-grid" container spacing={0.5}>
-                <Grid item xs={12} md={3}>
+            <div className="product-cards-container-grid" >
+                <div className="product-cards-container-card">
                     <ProductCard /> 
-                </Grid>
-                <Grid item xs={12} md={3}>
+                </div>
+                <div className="product-cards-container-card">
                     <ProductCard /> 
-                </Grid>
-                <Grid item xs={12} md={3}>
+                </div>
+                <div className="product-cards-container-card">
                     <ProductCard /> 
-                </Grid>
-                <Grid item xs={12} md={3}>
+                </div>
+                <div className="product-cards-container-card">
                     <ProductCard /> 
-                </Grid>
-            </Grid>
+                </div>
+                <div className="product-cards-container-card"> 
+                    <ProductCard /> 
+                </div>
+                <div className="product-cards-container-card">
+                    <ProductCard /> 
+                </div>
+                <div className="product-cards-container-card">
+                    <ProductCard /> 
+                </div>
+            </div>
         </div>
     )
 }
