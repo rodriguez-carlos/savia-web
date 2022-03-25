3f56281782cf1c42b94394fbfaa4f5fe6f9c4f94
unknown <carlosdanielrod@gmail.com>
2022-03-25 17:04:39 -0500

    no unused vars fix

--- src/components/PackAccordion.jsx
+++ src/components/PackAccordion.jsx
@@ -1,7 +1,6 @@
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import { useState } from 'react';
 
 
 const PackAccordion = ({size, expanded, handleChange}) => {
