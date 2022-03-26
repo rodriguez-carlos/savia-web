886f9183ff4293de34e7c95d15835e91b16ddf8b
unknown <carlosdanielrod@gmail.com>
2022-03-25 20:04:26 -0500

    pack mixto y acordeón

--- src/components/PackAccordion.jsx
+++ src/components/PackAccordion.jsx
@@ -1,19 +1,24 @@
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
+import "../styles/Components/_packaccordion.scss"
 
 const PackAccordion = ({size, expanded, handleChange}) => {
     
     return (
-        <Accordion expanded={expanded} onChange={handleChange}>
-            <AccordionSummary>
-                Pack Mixto x{size}
+        <Accordion className='accordion' expanded={expanded} onChange={handleChange}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className='accordion-summary-text'>Pack Mixto x{size}</Typography>
             </AccordionSummary>
             <AccordionDetails>
-                Se compone de<br />
-                x{size / 3} American Amber Ale,<br />
-                x{size / 3} Golden Ale,<br />
-                x{size / 3} Neipa.
+                <Typography variant="h5" className='accordion-summary-detail'>
+                    <strong>Se compone de</strong><br />
+                    x{size / 3} <strong>American Amber Ale,</strong><br />
+                    x{size / 3} <strong>Golden Ale,</strong><br />
+                    x{size / 3} <strong>Neipa</strong>
+                </Typography>
             </AccordionDetails>
         </Accordion>
     )
--- src/pages/BeerPack.jsx
+++ src/pages/BeerPack.jsx
@@ -54,42 +54,65 @@ const BeerPack = () => {
           <h1 className="title">{variety ? variety.attributes.nombre_corto : ''}</h1>
           <p className="subtitle">{variety ? variety.attributes.nombre_largo : ''}</p>
 
-          <p className="packs-name">Pack Botella de Vidrio</p>
           <p className="text">
             {variety ? variety.attributes.descripcion : ''}
           </p>
-          <PackAccordion size={6} expanded={expanded === 'panel1'} handleChange={handleChange('panel1')}/>
+
+          <PackAccordion
+            size={6}
+            expanded={expanded === 'panel1'}
+            handleChange={handleChange('panel1')}
+          />
           <p className="precio">
             <span><strong>Precio: </strong>
               {variety ? formatter.format(variety.attributes.precio_x6) : ''} CLP
             </span>
           </p>
-          <a href={variety ? `https://wa.me/56952321116?text=${messageGenerator(variety.attributes.nombre_corto, 6)}`: ''}
-          style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+          <a href={variety
+            ? `https://wa.me/56952321116?text=${messageGenerator(variety.attributes.nombre_corto, 6)}`
+            : ''}
+            style={{textDecoration: "none"}} target="_blank" rel="noreferrer"
+          >
             <Button id="button" sx={{ borderRadius: 15, marginTop: 1 }}>
               comprar pack x6
             </Button>
           </a>
-          <PackAccordion size={12} expanded={expanded === 'panel2'} handleChange={handleChange('panel2')}/>
+
+          <PackAccordion
+            size={12}
+            expanded={expanded === 'panel2'}
+            handleChange={handleChange('panel2')}
+          />
           <p className="precio">
             <span><strong>Precio: </strong>
               {variety ? formatter.format(variety.attributes.precio_x12) : ''} CLP
             </span>
           </p>
-          <a href={variety ? `https://wa.me/56952321116?text=${messageGenerator(variety.attributes.nombre_corto, 12)}`: ''}
-          style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+          <a href={variety
+            ? `https://wa.me/56952321116?text=${messageGenerator(variety.attributes.nombre_corto, 12)}`
+            : ''}
+            style={{textDecoration: "none"}} target="_blank" rel="noreferrer"
+          >
             <Button id="button" sx={{ borderRadius: 15, marginTop: 1 }}>
               comprar pack x12
             </Button>
           </a>
-          <PackAccordion size={24} expanded={expanded === 'panel3'} handleChange={handleChange('panel3')}/>
+
+          <PackAccordion
+            size={24}
+            expanded={expanded === 'panel3'}
+            handleChange={handleChange('panel3')}
+          />
           <p className="precio">
             <span><strong>Precio: </strong>
               {variety ? formatter.format(variety.attributes.precio_x24) : ''} CLP
             </span>
           </p>
-          <a href={variety ? `https://wa.me/56952321116?text=${messageGenerator(variety.attributes.nombre_corto, 24)}`: ''}
-          style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+          <a href={variety
+            ? `https://wa.me/56952321116?text=${messageGenerator(variety.attributes.nombre_corto, 24)}`
+            : ''}
+            style={{textDecoration: "none"}} target="_blank" rel="noreferrer"
+          >
             <Button id="button" sx={{ borderRadius: 15, marginTop: 1 }}>
               comprar pack x24
             </Button>
